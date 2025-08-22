22a9e726805b12b747063df5c30e9d1a71e9de3c
Lebanvla <144983462+Lebanvla@users.noreply.github.com>
2025-08-22 21:32:54 +0300

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,2 @@
+sudo pacman -Syu sudo pacman -S hyprland waybar swaybg mako foot cliphist grim slurp playerctl pavucontrol nautilus firefox code ttf-firacode-nerd ttf-font-awesome noto-fonts wofi
+chmod +x ~/.config/hypr/scripts/system-info.sh chmod +x ~/.config/hypr/scripts/waybar-update.sh chmod +x ~/.config/hypr/scripts/power-menu.sh
